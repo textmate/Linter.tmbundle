21f7d5117e3e1d6dcdbced6be97402897153f5b8
Mike McQuaid <mike@mikemcquaid.com>
2018-01-08 20:38:15 +0000

    json: use Ruby JSON module if no jsonlint.

--- Support/lib/languages/json.rb
+++ Support/lib/languages/json.rb
@@ -2,6 +2,23 @@ module Linter
   module_function
 
   def json
+    if which("jsonlint")
+      jsonlint
+    else
+      ruby_json
+    end
+  end
+
+  def ruby_json
+    /(?<version>\d+\.\d+\.\d+(p\d+)?)/ =~ `#{which("ruby")} --version`
+    {
+      name: "Ruby (JSON module)",
+      version: version,
+      output_command: "'#{which("ruby")}' -rjson -e'begin JSON.parse(IO.read(ARGV.first)); rescue => e; puts e; end' test.json",
+    }
+  end
+
+  def jsonlint
     {
       name: "JSONLint",
       version: `'#{which("jsonlint")}' --version`,
